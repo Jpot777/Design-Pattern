744d31d01ce7ac745e54e4be61d80f0362c68b2a
YMan95 <psymchoi@gmail.com>
2021-12-14 19:58:48 +0900

    #07 GOF - Facade Pattern
    Facade(퍼사드) 패턴
    - Facade클래스를 외부에서 호출함으로써 복잡하게 얽힌 클래스들을 간단하게 사용가능하게 해준다.

---
+++ Facade_Pattern/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Facade_Pattern
+{
+    // === A동작을 위한 클래스 === //
+    class SubSystemOne 
+    {
+        public void MethodOne() 
+        {
+            Console.WriteLine(" SubSystemOne Method");
+        }
+    }
+ 
+    // === B동작을 위한 클래스 === //
+    class SubSystemTwo 
+    {
+        public void MethodTwo() 
+        {
+            Console.WriteLine(" SubSystemTwo Method");
+        }
+    }
+ 
+    // === C동작을 위한 클래스 === //
+    class SubSystemThree 
+    {
+        public void MethodThree() 
+        {
+            Console.WriteLine(" SubSystemThree Method");
+        }
+    }
+ 
+    // === D동작을 위한 클래스 === //
+    class SubSystemFour 
+    {
+        public void MethodFour() 
+        {
+            Console.WriteLine(" SubSystemFour Method");
+        }
+    }
+    
+    class Facade 
+    {
+        private SubSystemOne one;
+        private SubSystemTwo two;
+        private SubSystemThree three;
+        private SubSystemFour four;
+ 
+        public Facade() 
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+        }
+ 
+        public void MethodA() 
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+ 
+        public void MethodB() 
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            two.MethodTwo();
+            three.MethodThree();
+        }
+    }
+ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+    
+            Console.ReadKey();
+        }
+    }
+}
